4b76c2d72feb1d958a07f588cd24453c5aaef6c9
Anthony Accioly <a.accioly@7rtc.com>
2019-06-26 00:48:07 +0100

    Setup babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as lib from './lib.js';
+
+console.log(lib.PI);            // 3.1415926
+console.log(lib.sum(1, 2, 3, 4));  // 10
+console.log(lib.mult(1, 2, 3, 4)); // 24
+console.log(lib.mult(1, 2, 3, 5)); // 30
